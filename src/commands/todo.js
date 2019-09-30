7ccf2890a0e3a41fb556570aadc2a9cb99473ee1
mskyberg <mskyberg@gmail.com>
2019-09-30 13:43:21 -0500

    added todo list response when adding new todo

--- src/commands/todo.js
+++ src/commands/todo.js
@@ -28,6 +28,41 @@ module.exports = {
             await keyv.set(server_id, todo_list.Serialize());
             info_message.embed.description = `${message.author.username} has added: ${todo_name}`
             message.channel.send(info_message);
+
+            // Update the todos and show the list. This seems dirty.
+            todo_list_json = await keyv.get(server_id);
+            todo_list.Deserialize(todo_list_json);
+
+            for (let [key, value] of todo_list.GetTodos())
+            {
+                const todo_message = {
+                    embed: {
+                        color: 12652005,
+                        description: "",
+                        author: {
+                            name: "",
+                            icon_url: ""
+                        },
+                        provider: {
+                            name: ""
+                        }
+                    }
+                };
+
+                let [author, avatar_url] = value.Author();
+                todo_message.embed.color = value.Color();
+                todo_message.embed.author.name = key;
+                todo_message.embed.author.icon_url = avatar_url;
+                for (let participant of value.Participants())
+                {
+                    // TODO (Garrett): Trailing comma
+                    todo_message.embed.description += participant + ",";
+                }
+                message.channel.send(todo_message).then( async message => {
+                    await message.react("✅");
+                    await message.react("❌");
+                });
+            }
         }
         return;
     },
