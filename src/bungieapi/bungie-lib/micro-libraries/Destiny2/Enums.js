f308b8ff668ad4ba592109abe925a3f2853274fa
mskyberg <mskyberg@gmail.com>
2019-11-19 16:02:27 -0600

    fixed CharacterProgressions component name

--- src/bungieapi/bungie-lib/micro-libraries/Destiny2/Enums.js
+++ src/bungieapi/bungie-lib/micro-libraries/Destiny2/Enums.js
@@ -39,7 +39,7 @@ const destinyComponentType = {
 	/** This will get you information about any non-equipped items on the character or character(s) in question, if you're allowed to see it. You have to either be authenticated as that user, or that user must allow anonymous viewing of their non-equipped items in Bungie.Net settings to actually get results. */
 	CHARACTERINVENTORIES: 201,
 	/** This will get you information about the progression (faction, experience, etc... "levels") relevant to each character, if you are the currently authenticated user or the user has elected to allow anonymous viewing of its progression info. */
-	CHARACTERPROGRESSSIONS : 202,
+	CHARACTERPROGRESSIONS : 202,
 	/** This will get you just enough information to be able to render the character in 3D if you have written a 3D rendering library for Destiny Characters, or "borrowed" ours. It's okay, I won't tell anyone if you're using it. I'm no snitch. (actually, we don't care if you use it - go to town) */
 	CHARACTERRENDERDATA : 203,
 	/** This will return info about activities that a user can see and gating on it, if you are the currently authenticated user or the user has elected to allow anonymous viewing of its progression info. Note that the data returned by this can be unfortunately problematic and relatively unreliable in some cases. We'll eventually work on making it more consistently reliable. */
