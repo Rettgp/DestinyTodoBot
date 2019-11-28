e3c5a425a70bfa90c5688bea74039898fc9c121c
mskyberg <mskyberg@gmail.com>
2019-11-27 20:11:39 -0600

    refactored todotimeout

--- src/TodoTimeout.js
+++ src/TodoTimeout.js
@@ -1,7 +1,4 @@
-import TodoEntry from "./TodoEntry";
 import TodoList from "./TodoList";
-import { ReactionEmoji } from 'discord.js';
-import { BungieApi } from "./bungieapi/BungieApi"
 import ColorCode from './Color';
 
 let active_timers = new Map();
@@ -23,77 +20,41 @@ export default class TodoTimeout
     async SetToDoTimeout(discord_guild)
     {
         let todo_list_json = await this.keyv.get(this.server_id);
-        if (todo_list_json === undefined)
-        {
-            console.log(`todo_list_json_undefined`);
-            return;
-        }
-
         let todo_list = new TodoList();
         todo_list.Deserialize(todo_list_json);
-        
-        for (let [key, value] of todo_list.GetTodos())
-        {
-            if (active_timers.has(key))
-            {
-                //console.log(`${key} timeout already exists, do not add another.`);
-                continue;
-            }            
-    
-            const info_message = {
-                embed: {
-                    description: "",
-                    color: ColorCode.DEFAULT,
-                    fields: [],
-                }
-            };
-    
-            let now = Date.now();
 
-            let reminder_timer = (value.Expiration() - now) - TWO_HOUR_OFFSET_MS; //remind 2 hours before expiration
-            if (value.Date() != "")
+        for (let [key, todo_entry] of todo_list.GetTodos())
+        {
+            if (active_timers.has(this.key))
             {
-                reminder_timer = (value.Date() - now) - TWO_HOUR_OFFSET_MS; // remind 2 hours before activity date
+                //console.log(`${this.key} timeout already exists, do not add another.`);
+                continue;
+            }          
+
+            if (this.key !== key)
+            {
+                //console.log(`${this.key} does not match ${key}.`);
+                continue;
+            }
+
+            let now = Date.now();
+            let reminder_timer = (todo_entry.Expiration() - now) - TWO_HOUR_OFFSET_MS; //remind 2 hours before expiration
+            if (todo_entry.Date() != "")
+            {
+                reminder_timer = (todo_entry.Date() - now) - TWO_HOUR_OFFSET_MS; // remind 2 hours before activity date
                 if (reminder_timer < 0)
                 {
                     // if the activity date is less than two hours from now, take half the difference.
-                    reminder_timer = ((value.Date() - now) / 2);
+                    reminder_timer = ((todo_entry.Date() - now) / 2);
                 }
             }
-            let expiration_timer = (value.Expiration() - now) - TWENTY_FOUR_HOUR_OFFSET_MS;
-            var reminder_timeout = setTimeout(() => {
-                info_message.embed.description = `${key} Activity Reminder: ${new Date(value.Date())}`;
-                info_message.embed.fields = [];
-                let mention_participants = "";
-                for (let participant of value.Participants())
-                {
-                    let discord_guildmember = discord_guild.members.find(val => {return val.user.username === participant});
-                    let discord_user_id = discord_guildmember.user.id;
-                    mention_participants += `<@${discord_user_id}> `;
-                }
-                info_message.embed.fields.push({ name: `Participants:`, value: `${mention_participants}` });
-                this.message.channel.send(info_message);
-              }, reminder_timer);
-              
+            let expiration_timer = (todo_entry.Expiration() - now);
     
-            var expiration_timeout = setTimeout(() => {
-                this.CleanUpTodo();
-
-                info_message.embed.description = `${key} Activity Expired: ${new Date(Date.now())}`;
-                info_message.embed.fields = [];
-                let mention_participants = "";
-                for (let participant of value.Participants())
-                {
-                    let discord_guildmember = discord_guild.members.find(val => {return val.user.username === participant});
-                    let discord_user_id = discord_guildmember.user.id;
-                    mention_participants += `<@${discord_user_id}> `;
-                }
-                info_message.embed.fields.push({ name: `Participants:`, value: `${mention_participants}` });
-                this.message.channel.send(info_message);
-              }, expiration_timer);
+            let reminder_timeout = setTimeout(() => { this.SetTodoReminderTimeout(discord_guild)}, reminder_timer);
+            let expiration_timeout = setTimeout(() =>  {this.SetToDoExpirationTimeout(discord_guild) }, expiration_timer);
     
-            active_timers.set( key, {reminder_timeout,expiration_timeout});
-        }        
+            active_timers.set( this.key, {reminder_timeout,expiration_timeout});    
+        }
     }
 
     async RemoveToDoTimeout()
@@ -112,12 +73,6 @@ export default class TodoTimeout
     async CleanUpTodo()
     {
         let todo_list_json = await this.keyv.get(this.server_id);
-        if (todo_list_json === undefined)
-        {
-            console.log(`Could not obtain todo_list_json.`);
-            return;
-        }
-
         let todo_list = new TodoList();
         todo_list.Deserialize(todo_list_json);
 
@@ -125,6 +80,75 @@ export default class TodoTimeout
         {
             todo_list.RemoveTodo(this.key);
             await this.keyv.set(this.server_id, todo_list.Serialize());
+        }
+    }
+
+    async SetTodoReminderTimeout(discord_guild)
+    {
+        const info_message = {
+            embed: {
+                description: "",
+                color: ColorCode.DEFAULT,
+                fields: [],
+                }
+            };
+
+        let todo_list_json = await this.keyv.get(this.server_id);
+        let todo_list = new TodoList();
+        todo_list.Deserialize(todo_list_json);
+        for (let [key, todo_entry] of todo_list.GetTodos())
+        {
+            if (this.key !== key)
+            {
+                //console.log(`${this.key} does not match ${key}.`);
+                continue;
+            }
+            info_message.embed.description = `${this.key} Activity Reminder: ${new Date(todo_entry.Date())}`;
+            info_message.embed.fields = [];
+            let mention_participants = "";
+            for (let participant of todo_entry.Participants())
+            {
+                let discord_guildmember = discord_guild.members.find(val => {return val.user.username === participant});
+                let discord_user_id = discord_guildmember.user.id;
+                mention_participants += `<@${discord_user_id}> `;
+            }
+            info_message.embed.fields.push({ name: `Participants:`, value: `${mention_participants}` });
+            this.message.channel.send(info_message);
+        }
+    }
+
+    async SetToDoExpirationTimeout(discord_guild)
+    {
+        const info_message = {
+            embed: {
+                description: "",
+                color: ColorCode.DEFAULT,
+                fields: [],
+                }
+            };
+
+        let todo_list_json = await this.keyv.get(this.server_id);
+        let todo_list = new TodoList();
+        todo_list.Deserialize(todo_list_json);
+        for (let [key, todo_entry] of todo_list.GetTodos())
+        {
+            if (this.key !== key)
+            {
+                //console.log(`${this.key} does not match ${key}.`);
+                continue;
+            }
+            info_message.embed.description = `${this.key} Activity Expired: ${new Date(Date.now())}`;
+            info_message.embed.fields = [];
+            let mention_participants = "";
+            for (let participant of todo_entry.Participants())
+            {
+                let discord_guildmember = discord_guild.members.find(val => {return val.user.username === participant});
+                let discord_user_id = discord_guildmember.user.id;
+                mention_participants += `<@${discord_user_id}> `;
+            }
+            this.CleanUpTodo();
+            info_message.embed.fields.push({ name: `Participants:`, value: `${mention_participants}` });
+            this.message.channel.send(info_message);
         }
     }
 }
